60702ff9bf1f8e6114a766acbb1061d920c4d6e3
ssolis1337 <71684140+ssolis86@users.noreply.github.com>
2021-06-16 14:23:47 -0500

    render option for user to choose inquirer

--- server.js
+++ server.js
@@ -2,27 +2,103 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTables = require('console.table');
 
-const connection = mysql.connection({
+const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Loeffler1',
-    database: 'Employee_DB',
+    password: 'Loeffler1986',
+    database: 'employeeDB',
 });
 
 connection.connect((error ) => {
     if (error) throw error;
     console.log(`Connected: + ${connection.threadId}`);
 
-    initiate();
+    run();
 })
 
-const initiate = () => {
-    inquirer.prompt(
-        {
-            type: 'list',
-            message: 'What do you need?',
-            choices: ['View Employees', 'View Employees by Department']
+const  run = () => {
+    inquirer
+      .prompt({
+        name: "option",
+        type: "list",
+        message: "Do you need to [ADD DEPARTMENT] , [ADD ROLE] , [ADD EMPLOYEE], [VIEW DEPARTMENT], [VIEW ROLE], [VIEW EMPLOYEES], [UPDATE EMPLOYEE ROLES] ",
+        choices: ['ADD DEPARTMENT' , 'ADD ROLE' , 'ADD EMPLOYEE', 'VIEW DEPARTMENT', 'VIEW ROLE', 'VIEW EMPLOYEES', 'UPDATE EMPLOYEE ROLES'],
+      })
+      .then((answer) => {
+        switch (answer.option) {
+            case "ADD DEPARTMENT":
+                runAddDep();
+                break;
+            case "ADD ROLE":
+                runAddRole();
+                break;
+            case "ADD EMPLOYEE":
+                runAddEmp();
+                break;
+            case "VIEW DEPARTMENT":
+                runViewDep();
+                break;
+            case "VIEW ROLE":
+                runViewRole();
+                break;
+            case "VIEW EMPLOYEES":
+                runViewEmp();
+                break;
+            case "UPDATE EMPLOYEE ROLES":
+                runUpdEmpRls();
+                break;
         }
-    )
+    });
+};
+
+runAddDep = () => {
+    console.log('runAddDep');
+
+
+
+    run();
+}
+
+runAddRole = () => {
+    console.log('runAddRole');
+
+
+
+    run();
+}
+
+runAddEmp = () => {
+    console.log('runAddEmp');
+
+
+    run();
+}
+
+runViewDep = () => {
+    console.log('runViewDep');
+
+
+    run();
+}
+
+runViewRole = () => {
+    console.log('runViewRole');
+
+
+    run();
+}
+
+runViewEmp = () => {
+    console.log('runViewEmp');
+
+
+    run();
+}
+
+runUpdEmpRls = () => {
+    console.log('runEmpRls');
+
+
+    run();
 }
